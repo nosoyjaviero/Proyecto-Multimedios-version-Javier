57001a41154bb351bb4bf6968d36a9c550b89a72
nosoyjaviero <lopez4935@gmail.com>
2023-06-16 16:40:45 -0600

    Carousel con sus propiedades Background Wallpaper agregado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import { useState, React } from 'react';
+
+// estaticos
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css' // or less ideally
-import { Button } from 'react-bootstrap';
+import background from "./media/background.jpeg";
+
+// archivos
+import ControlledCarousel from './Carousel/Carousel';
+
+// bootstrap
+// import { Button } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
         <div className='navar'>
         </div>
+
+        
         <main className='Carousel'>
+         <div className='carousel-content contenedor'>
+          <ControlledCarousel></ControlledCarousel>         
+         </div>
         </main>
     </div>
   );
