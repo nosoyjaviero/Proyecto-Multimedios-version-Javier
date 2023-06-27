358cb24e813ed395e0ab1282278384fc9af91333
Briones-cr <130931347+Briones-cr@users.noreply.github.com>
2023-06-26 22:02:16 -0600

    barra de busqueda agregada correctamente

--- src/componentes navar/head.jsx
+++ src/componentes navar/head.jsx
@@ -1,9 +1,14 @@
+import React, { useState, useRef } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
+import Form from 'react-bootstrap/Form';
 import './stylecomponentes.css';
-
-const Head = () => {
+function Head() {
+    const [show, setShow] = useState(false);
+    const target = useRef(null);
     return (
         <Container fluid className="custom-container">
             <Row className="custom-row">
@@ -60,16 +65,39 @@ const Head = () => {
                     </div>
                 </Col>
                 <Col className="custom-col" xl={3} >
-                    <div className='signIn'>
-                        <button className='buscador'>
+                    <div className='buscador'>
+                        <Button variant="danger" ref={target} onClick={() => setShow(!show)} style={{ paddingTop: '25px', display: 'flex', background: 'transparent', border: 'none' }}>
                             <svg xmlns="http://www.w3.org/2000/svg" id='lupa' class="icon icon-tabler icon-tabler-search" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
                                 <path d="M21 21l-6 -6" />
                             </svg>
-                        </button>
-                        <p>SEARCH</p>
-                        <button className='buscador'>
+                            <p>SEARCH</p>
+                        </Button>
+                        <Overlay target={target.current} show={show} placement="left">
+                            {({ placement, arrowProps, show: _show, popper, ...props }) => (
+                                <div
+                                    {...props}
+                                    style={{
+                                        position: 'absolute',
+                                        padding: '10px 0px',
+                                        justifyItems: 'center',
+                                        borderRadius: 3,
+                                        ...props.style,
+                                    }}
+                                >
+                                    <Form className="d-flex">
+                                        <Form.Control
+                                            type="search"
+                                            placeholder="Search"
+                                            className="me-2"
+                                            aria-label="Search"
+                                        />
+                                    </Form>
+                                </div>
+                            )}
+                        </Overlay>
+                        <button className='signIn'>
                             <svg xmlns="http://www.w3.org/2000/svg" id='people' class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
